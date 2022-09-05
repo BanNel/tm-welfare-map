3fb7371fd428c664af676afb96c341da2604a73f
Yu Chun Tsao <tsao84672776@gmail.com>
2022-09-05 09:26:43 +0800

    Add GoHomeControl
    Custom control class

--- src/components/Map/MapView.js
+++ src/components/Map/MapView.js
@@ -3,17 +3,19 @@ import { useSelector, useDispatch } from "react-redux";
 import { mapActions } from "../../store/map-slice";
 import { fetchPoiGeojson } from "../../store/map-actions";
 
+import "./MapView.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import Map, { NavigationControl, ScaleControl } from "react-map-gl";
 import icons from "../../utils/icons";
 import PoiLayer from "./Layer/PoiLayer";
 import CompanyLayer from "./Layer/CompanyLayer";
+import GoHomeContorl from "./Control/GoHomeControl";
 
 // In order to solve the bug in production: Uncaught ReferenceError: y is not defined
 // import maplibregl from "maplibre-gl";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import maplibregl from '!maplibre-gl';      // ! is important here
-import maplibreglWorker from 'maplibre-gl/dist/maplibre-gl-csp-worker';
+import maplibregl from "!maplibre-gl"; // ! is important here
+import maplibreglWorker from "maplibre-gl/dist/maplibre-gl-csp-worker";
 maplibregl.workerClass = maplibreglWorker;
 
 const MapView = () => {
@@ -65,6 +67,14 @@ const MapView = () => {
       >
         <NavigationControl position="bottom-right" />
         <ScaleControl />
+        <GoHomeContorl
+          position="bottom-right"
+          displayControlsDefault={false}
+          className="mapboxgl-ctrl-go-home"
+          title="Go To Trend Micro"
+          lnglat={[viewState.longitude, viewState.latitude]}
+          zoom={viewState.zoom}
+        />
 
         {/* Custom layers */}
         <PoiLayer />
