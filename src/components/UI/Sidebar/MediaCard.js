18dd1d8a06fac59a43db99f41e11e6fd39c6082c
Yu Chun Tsao <tsao84672776@gmail.com>
2022-09-19 17:44:22 +0800

    Update media icon and link
    - If link value is null, hidden Link components
    - Add tooltip for icon
    - Update contract date

--- src/components/UI/Sidebar/MediaCard.js
+++ src/components/UI/Sidebar/MediaCard.js
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import Link from "@mui/material/Link";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -12,9 +13,20 @@ import DiscountIcon from "@mui/icons-material/Discount";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import ClassChips from "./ClassChips";
 import OpenWithGoogleMaps from "./OpenWithGoogleMaps";
+import DescriptionIcon from "@mui/icons-material/Description";
+import ShareIcon from "@mui/icons-material/Share";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function MediaCard(props) {
   // TODO: focus feature 更新後，初始化 Card scroll。
+
+  const contract_date =
+    props.feature.properties.start_date === undefined
+      ? props.feature.properties.expiration
+      : props.feature.properties.start_date +
+        " - " +
+        props.feature.properties.expiration;
+
   return (
     <Card
       sx={{ overflow: "auto", width: "100%", height: "100%", borderRadius: 0 }}
@@ -36,45 +48,67 @@ export default function MediaCard(props) {
 
         <List>
           <ListItem disablePadding>
-              <ListItemIcon>
+            <ListItemIcon>
+              <Tooltip title="地址" arrow>
                 <LocationOnIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.address} />
+              </Tooltip>
+            </ListItemIcon>
+            <ListItemText primary={props.feature.properties.address} />
           </ListItem>
-          <Divider sx={{margin:"10px 0px 10px 0px"}} component="li" />
+          <Divider sx={{ margin: "10px 0px 10px 0px" }} component="li" />
           <ListItem disablePadding>
-              <ListItemIcon>
+            <ListItemIcon>
+              <Tooltip title="優惠" arrow>
                 <DiscountIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.description} />
+              </Tooltip>
+            </ListItemIcon>
+            <ListItemText primary={props.feature.properties.description} />
           </ListItem>
-          <Divider sx={{margin:"10px 0px 10px 0px"}} component="li" />
+          <Divider sx={{ margin: "10px 0px 10px 0px" }} component="li" />
           <ListItem disablePadding>
-              <ListItemIcon>
+            <ListItemIcon>
+              <Tooltip title="合約到期日" arrow>
                 <DateRangeIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.expiration} />
+              </Tooltip>
+            </ListItemIcon>
+            <ListItemText primary={contract_date} />
           </ListItem>
-          <Divider sx={{margin:"10px 0px 10px 0px"}} component="li" />
+          <Divider sx={{ margin: "10px 0px 10px 0px" }} component="li" />
           <ListItem disablePadding>
-              <ListItemIcon>
-                <DateRangeIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.start_date} />
+            <ListItemIcon>
+              <Tooltip title="店家連結" arrow>
+                <ShareIcon color="primary" />
+              </Tooltip>
+            </ListItemIcon>
+            {props.feature.properties.shop_url !== undefined && (
+              <Link
+                href={props.feature.properties.shop_url}
+                target="_blank"
+                rel="noopener"
+                underline="none"
+              >
+                <ListItemText primary="店家連結" />
+              </Link>
+            )}
           </ListItem>
-          <Divider sx={{margin:"10px 0px 10px 0px"}} component="li" />
+          <Divider sx={{ margin: "10px 0px 10px 0px" }} component="li" />
           <ListItem disablePadding>
-              <ListItemIcon>
-                <DateRangeIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.shop_url} />
-          </ListItem>
-          <Divider sx={{margin:"10px 0px 10px 0px"}} component="li" />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <DateRangeIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={props.feature.properties.contract_url} />
+            <ListItemIcon>
+              <Tooltip title="合約連結" arrow>
+                <DescriptionIcon color="primary" />
+              </Tooltip>
+            </ListItemIcon>
+
+            {props.feature.properties.contract_url !== undefined && (
+              <Link
+                href={props.feature.properties.contract_url}
+                target="_blank"
+                rel="noopener"
+                underline="none"
+              >
+                <ListItemText primary="合約連結" />
+              </Link>
+            )}
           </ListItem>
         </List>
       </CardContent>
