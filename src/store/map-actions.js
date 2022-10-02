dc3de6f6b106890c36f663ed2125ee66049eb0dc
Yu Chun Tsao <tsao84672776@gmail.com>
2022-10-02 23:15:23 +0800

    Reset duplicate coordinates
    Based on number of features to generate regular polygon.
    Use nodes of regular polygon as a new coordinates.

--- src/store/map-actions.js
+++ src/store/map-actions.js
@@ -1,6 +1,68 @@
 import { mapActions } from "./map-slice";
 import poiGeojsonUrl from "../assets/data/poi.geojson";
 import companyGeojsonUrl from "../assets/data/company.geojson";
+import * as turf from "@turf/turf";
+
+const arraysEqual = (a, b) =>
+  a.length === b.length && a.every((v, i) => v === b[i]);
+
+const getDuplicateCoordinates = (features) => {
+  let duplicate_coordinates = [];
+
+  features.forEach((f1) => {
+    if (f1.geometry === null) return;
+    features.forEach((f2) => {
+      if (f2.geometry === null) return;
+      if (f1 === f2) return;
+
+      let f1_coords = f1.geometry.coordinates;
+      let f2_coords = f2.geometry.coordinates;
+      let distance = turf.distance(f1_coords, f2_coords);
+
+      // If distance equal to zero, the coordinates of f1 and f2 are the same.
+      if (distance === 0) {
+        // Check the coordinates no in duplicate_coordinates array.
+        let index = duplicate_coordinates.findIndex(
+          (c) => c[0] === f1_coords[0] && c[1] === f1_coords[1]
+        );
+
+        let coordinatesNoInArray = index === -1;
+        if (coordinatesNoInArray) {
+          duplicate_coordinates.push(f1_coords);
+        }
+      }
+    });
+  });
+
+  return duplicate_coordinates;
+};
+
+const resetDuplicatieCoordinates = (features, duplicate_coordinates) => {
+  duplicate_coordinates.forEach((dc) => {
+    let centerPoint = turf.point(dc);
+    let radius = 0.015; // kilometers
+
+    let targetFeatures = features.filter((f) => {
+      if (f.geometry === null) return false;
+      return arraysEqual(f.geometry.coordinates, dc);
+    });
+
+    // Generate regular polygons based on the number of features
+    // Each LinearRing of a Polygon must have 4 or more Positions.
+    let numberOfFeatures =
+      targetFeatures.length < 4 ? 4 : targetFeatures.length;
+
+    let regularPolygon = turf.circle(centerPoint, radius, {
+      steps: numberOfFeatures,
+    });
+
+    // Use polygon nodes as new coordinates.
+    targetFeatures.forEach((feature, index) => {
+      feature.geometry.coordinates =
+        regularPolygon.geometry.coordinates[0][index];
+    });
+  });
+};
 
 export const fetchPoiGeojson = () => {
   return async (dispatch) => {
@@ -17,10 +79,22 @@ export const fetchPoiGeojson = () => {
 
     try {
       const poiGeojson = await fetchData();
-      const reversePoiGeojson = {
+      let reversePoiGeojson = {
         ...poiGeojson,
         features: poiGeojson.features.reverse(),
       };
+
+      // Get duplicate coordinates in poi features.
+      let duplicate_coordinates = getDuplicateCoordinates(
+        reversePoiGeojson.features
+      );
+
+      // Reset duplicate coordinates with nodes of regular polygon
+      resetDuplicatieCoordinates(
+        reversePoiGeojson.features,
+        duplicate_coordinates
+      );
+
       dispatch(mapActions.setPoiGeojson(reversePoiGeojson));
     } catch (error) {
       // TODO: ui error notification
