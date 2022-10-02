8e0bfb21a4823adae6aa415e571cf1a3eb102564
Yu Chun Tsao <tsao84672776@gmail.com>
2022-10-02 13:51:37 +0800

    Update POI symbol icon and color

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,4 +1,3 @@
-import beer from "../assets/icons/beer.svg";
 import bread from "../assets/icons/bread.svg";
 import burger from "../assets/icons/burger.svg";
 import cafe from "../assets/icons/cafe.svg";
@@ -6,7 +5,6 @@ import cake from "../assets/icons/cake.svg";
 import car from "../assets/icons/car.svg";
 import clothes from "../assets/icons/clothes.svg";
 import drink from "../assets/icons/drink.svg";
-import fruit from "../assets/icons/fruit.svg";
 import general_restaurant from "../assets/icons/general_restaurant.svg";
 import home from "../assets/icons/home.svg";
 import japanese_restaurant from "../assets/icons/japanese_restaurant.svg";
@@ -17,12 +15,12 @@ import shop from "../assets/icons/shop.svg";
 import sports from "../assets/icons/sports.svg";
 import trend_micro from "../assets/icons/trend_micro.svg";
 import wine from "../assets/icons/wine.svg";
+import travel from "../assets/icons/travel.svg";
 import selected_icon from "../assets/icons/selected_icon.svg";
 import simple_circle from "../assets/icons/simple_circle.svg"
 
 // Collect all icons into an object
 const icons = {
-  beer: { file: beer, width: 48, height: 48, sdf: false },
   bread: { file: bread, width: 48, height: 48, sdf: false },
   burger: { file: burger, width: 48, height: 48, sdf: false },
   cafe: { file: cafe, width: 48, height: 48, sdf: false },
@@ -30,7 +28,6 @@ const icons = {
   car: { file: car, width: 48, height: 48, sdf: false },
   clothes: { file: clothes, width: 48, height: 48, sdf: false },
   drink: { file: drink, width: 48, height: 48, sdf: false },
-  fruit: { file: fruit, width: 48, height: 48, sdf: false },
   general_restaurant: {
     file: general_restaurant,
     width: 48,
@@ -51,6 +48,7 @@ const icons = {
   sports: { file: sports, width: 48, height: 48, sdf: false },
   trend_micro: { file: trend_micro, width: 48, height: 48, sdf: false },
   wine: { file: wine, width: 48, height: 48, sdf: false },
+  travel: { file: travel, width: 48, height: 48, sdf: false },
   selected_icon: { file: selected_icon, width: 48, height: 48, sdf: false },
   simple_circle: { file: simple_circle, width: 48, height: 48, sdf: true }
 };
