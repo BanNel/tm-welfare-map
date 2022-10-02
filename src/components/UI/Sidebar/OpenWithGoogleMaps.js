32618479e99a47ce2b0231e36688ea5bfbe4f251
Win10 <bannel@home.com>
2022-10-02 15:29:03 +0800

    update poi.geojson & add click POI event tracker

--- src/components/UI/Sidebar/OpenWithGoogleMaps.js
+++ src/components/UI/Sidebar/OpenWithGoogleMaps.js
@@ -1,10 +1,16 @@
 import { Chip } from "@mui/material";
 import { Fragment } from "react";
 import PushPinIcon from "@mui/icons-material/PushPin";
+import ReactGA from 'react-ga4';
 
 const OpenWithGoogleMaps = (props) => {
   const url = `https://www.google.com/maps/search/?api=1&query=${props.name}`;
-
+  ReactGA.event({
+    category: "click",
+    action: "Click POI",
+    label: props.name, // optional
+  });
+  console.log("console log: Click POI");
   return (
     <Fragment>
       <Chip
